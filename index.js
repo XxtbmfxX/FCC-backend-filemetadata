56a6ca9bcb6967019730549397934dfec7bee640
T0R0_17 <aatn1321@gmail.com>
2024-03-08 17:16:46 +0000

    okeydokey

--- index.js
+++ index.js
@@ -2,7 +2,16 @@ var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
 
+const multer = require('multer');
+const fs = require('fs');
+
 var app = express();
+
+
+// Configuramos Express para analizar los datos de formularios
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,10 +20,25 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+// Configurar Multer para manejar la carga de archivos
+const storage = multer.memoryStorage(); // Almacenar el archivo en memoria
+const upload = multer({ storage: storage });
 
+// Ruta para la carga de archivos
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  // Obtener la información del archivo cargado
+  const fileInfo = {
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  };
 
+  // Retornar la información del archivo en formato JSON
+  res.json(fileInfo);
+});
 
-const port = process.env.PORT || 3000;
-app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+// Iniciar el servidor
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor en ejecución en el puerto ${PORT}`);
 });
